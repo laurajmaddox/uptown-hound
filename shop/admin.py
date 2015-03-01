b60ca184c81f6b4f7d43eb508d07f49522cf5cd7
Laura Maddox <ljgmaddox@gmail.com>
2015-02-28 20:43:29 -0800

    Update admin - add all models

--- shop/admin.py
+++ shop/admin.py
@@ -1,9 +1,11 @@
 from django.contrib import admin
-from shop.models import Product, ProdImage
+from shop.models import Product, ProdCategory, ProdImage
 
 class ProductAdmin(admin.ModelAdmin):
-    fields = ['name', 'description', 'slug', 'main_img', 'active']
+    fields = ['name', 'description', 'slug', 'main_img', 'active', 'category']
+    filter_horizontal = ['category']
     prepopulated_fields = {'slug': ('name',)}
 
 admin.site.register(Product, ProductAdmin)
+admin.site.register(ProdCategory)
 admin.site.register(ProdImage)
