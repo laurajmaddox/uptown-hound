e5fc5f4f42449ec98433520751cc1a5bca183fe3
Laura Maddox <ljgmaddox@gmail.com>
2015-02-28 21:40:28 -0800

    Bootstrap + create empty base template

--- shop/views.py
+++ shop/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+
+def index(request):
+    """
+    View for landing/home page
+    """
+    return render(request, 'index.html')
--- uptown_hound/urls.py
+++ uptown_hound/urls.py
@@ -1,10 +1,12 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
+from django.conf.urls.static import static
+
+from shop import views
+
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'uptown_hound.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
     url(r'^admin/', include(admin.site.urls)),
-)
+    url(r'^$', views.index, name='index'),
+) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
