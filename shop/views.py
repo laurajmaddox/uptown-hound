63f6ff07a7ca647c4fa9edfa4cac4725cf329215
Laura Maddox <ljgmaddox@gmail.com>
2015-03-03 14:15:53 -0800

    Add breadcrumbs to category view

--- shop/views.py
+++ shop/views.py
@@ -1,4 +1,6 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+
+from shop.models import ProdCategory
 
 
 def index(request):
@@ -6,3 +8,27 @@ def index(request):
     View for landing/home page
     """
     return render(request, 'index.html')
+
+def category(request, cat_slugs):
+    """
+    View for category browsing page
+    """
+    cat_slugs, crumbs = cat_slugs.split('/'), []
+
+    for i in range(len(cat_slugs)):
+        if not crumbs:
+            parent = None
+        else:
+            parent = crumbs[-1][0]
+        category = get_object_or_404(ProdCategory, slug=cat_slugs[i], parent=parent)
+        crumbs.append([category, '/'.join(cat_slugs[:i + 1])])
+
+    return render(request, 'category.html', {
+        'crumbs': crumbs,
+    })
+
+def product(request, product):
+    """
+    View for product detail page
+    """
+    return render(request, 'product.html')
--- uptown_hound/urls.py
+++ uptown_hound/urls.py
@@ -9,4 +9,6 @@ from shop import views
 urlpatterns = patterns('',
     url(r'^admin/', include(admin.site.urls)),
     url(r'^$', views.index, name='index'),
+    url(r'^category/(?P<cat_slugs>.+)/$', views.category, name='category'),
+    url(r'^product/(?P<product_slug>[\w-]+)/$', views.product, name='product'),
 ) + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
