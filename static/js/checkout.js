d70b576a6c733ebd89138b6fb033c15fc176661e
Laura Maddox <ljgmaddox@gmail.com>
2015-05-04 12:07:38 -0700

    Split payment JS into checkout.js

---
+++ static/js/checkout.js
@@ -0,0 +1,76 @@
+/*===============================================
+ checkout.js
+===============================================*/
+
+
+$(document).ready(function() {
+
+    // Create Stripe token
+    $.createStripeToken = function (cardNum, cvc, expObj) {
+        Stripe.card.createToken({
+            number: cardNum,
+            cvc: cvc,
+            exp_month: expObj.month,
+            exp_year: expObj.year
+
+        }, function (status, response) {
+            var $form = $('.js-checkout-payment');
+            
+            if (response.error) {                
+                $('.js-payment-errors').removeClass('hidden');
+                $('.js-payment-errors').text(response.error.message);
+                
+            } else {
+                var token = response.id;
+
+                // Add token as hidden field to form, then submit
+                $form.append($('<input type="hidden" name="payment-stripe_token" />').val(token));
+                $form.get(0).submit();
+            }                           
+        });
+    };
+
+    // Add or remove form errors in template
+    $.fn.toggleInputError = function (erred) {
+        var errorText = erred ? 
+            '<ul class="errorlist">Invalid card info, please check your input.<li></li></ul>' : '';
+        this.parent('.form-group').children('.text-danger').html(errorText);
+        return this;
+    };
+
+    // jquery.payment field formatting
+    $('#js-cc-number').payment('formatCardNumber');
+    $('#js-cc-exp').payment('formatCardExpiry');
+    $('#js-cc-cvc').payment('formatCardCVC');
+
+    // Validate form data & create token for payment processing
+    $('.js-checkout-payment').submit(function (event) {
+        
+        // Halt form submission
+        event.preventDefault();
+        // Remove error messages from any previous payment attempt
+        $('.js-payment-errors').addClass('hidden');
+
+        Stripe.setPublishableKey('pk_8Oa88NCeR2XVsrIm9uv1qf8QPoEt3');
+
+        var numVal = $.payment.validateCardNumber($('#js-cc-number').val());
+        var expVal = $.payment.validateCardExpiry($('#js-cc-exp').payment('cardExpiryVal'));
+        var cardType = $.payment.cardType($('#js-cc-number').val());
+        var cvcVal = $.payment.validateCardCVC($('#js-cc-cvc').val(), cardType);
+
+        // Toggle errors for valid/invalid input fields
+        $('#js-cc-number').toggleInputError(!numVal);
+        $('#js-cc-exp').toggleInputError(!expVal);
+        $('#js-cc-cvc').toggleInputError(!cvcVal);
+
+        // Create Token if form data is valid
+        if (numVal && expVal && cvcVal) {                        
+            $.createStripeToken(
+                $('#js-cc-number').val(),
+                $('#js-cc-cvc').val(),
+                $('#js-cc-exp').payment('cardExpiryVal')
+            );
+        }
+    });
+
+});
--- static/js/shop.js
+++ static/js/shop.js
@@ -1,3 +1,8 @@
+/*===============================================
+ base.js
+===============================================*/
+
+
 $(document).ready(function() {
   
     /* Add navbar shadow on page scroll */
@@ -57,55 +62,6 @@ $(document).ready(function() {
         var src = $(this).attr('src');
         $('#js-main-image').attr('src', src);
         $('.image-popup').attr('href', src);
-    });
-
-    /* Credit card jquery.payment functions */
-    $('#js-cc-number').payment('formatCardNumber');
-    $('#js-cc-exp').payment('formatCardExpiry');
-    $('#js-cc-cvc').payment('formatCardCVC');
-
-    $('.js-checkout-payment').submit(function (event) {
-        event.preventDefault();
-
-        /* Remove error messages from any previous payment attempt */
-        $('.js-payment-errors').addClass('hidden');
-
-        Stripe.setPublishableKey('pk_8Oa88NCeR2XVsrIm9uv1qf8QPoEt3');
-
-        $.fn.toggleInputError = function(erred) {
-            var errorText = erred ? 
-                '<ul class="errorlist">Invalid card info, please check your input.<li></li></ul>' : '';
-            this.parent('.form-group').children('.text-danger').html(errorText);
-            return this;
-        };
-        var numVal = $.payment.validateCardNumber($('#js-cc-number').val());
-        var expVal = $.payment.validateCardExpiry($('#js-cc-exp').payment('cardExpiryVal'));
-        var cvcVal = $.payment.validateCardCVC($('#js-cc-cvc').val(), cardType);
-
-        var cardType = $.payment.cardType($('#js-cc-number').val());
-        $('#js-cc-number').toggleInputError(!numVal);
-        $('#js-cc-exp').toggleInputError(!expVal);
-        $('#js-cc-cvc').toggleInputError(!cvcVal);
-
-        if (numVal && expVal && cvcVal) {
-            var expObject = $('#js-cc-exp').payment('cardExpiryVal');
-            Stripe.card.createToken({
-                number: $('#js-cc-number').val(),
-                cvc: $('#js-cc-cvc').val(),
-                exp_month: expObject.month,
-                exp_year: expObject.year
-            }, function (status, response) {
-                var $form = $('.js-checkout-payment');
-                if (response.error) {
-                    $('.js-payment-errors').removeClass('hidden');
-                    $('.js-payment-errors').text(response.error.message);
-                } else {
-                    var token = response.id;
-                    $form.append($('<input type="hidden" name="payment-stripe_token" />').val(token));
-                    $form.get(0).submit();
-                }                
-            });
-        }
     });
 
 });
