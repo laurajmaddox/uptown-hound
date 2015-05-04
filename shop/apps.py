63b53bfe973620f5f66dc24bd9f8ddf3fc63f5d1
Laura Maddox <ljgmaddox@gmail.com>
2015-05-04 09:30:55 -0700

    Comment apps.py

--- shop/apps.py
+++ shop/apps.py
@@ -1,13 +1,20 @@
+# ===============================================
+# shop/apps.py
+# ===============================================
+
+
 from django.apps import AppConfig
 import watson
 
 
 class ShopConfig(AppConfig):
     """
-    Extend AppConfig to register models to be included in search
+    Extend AppConfig to register models to be included in Watson search
     """
     name = 'shop'
     
     def ready(self):
         Product = self.get_model('Product')
-        watson.register(Product.objects.filter(active=True), fields=('description', 'name',))
+        watson.register(
+            Product.objects.filter(active=True), fields=('description', 'name',)
+        )
