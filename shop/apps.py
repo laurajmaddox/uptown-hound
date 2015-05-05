ab53289eb3c657ce3df602e3336340ea8a812fc5
Laura Maddox <ljgmaddox@gmail.com>
2015-05-05 16:18:48 -0700

    Pep8 apps.py

--- shop/apps.py
+++ shop/apps.py
@@ -12,9 +12,10 @@ class ShopConfig(AppConfig):
     Extend AppConfig to register models to be included in Watson search
     """
     name = 'shop'
-    
+
     def ready(self):
         Product = self.get_model('Product')
         watson.register(
-            Product.objects.filter(active=True), fields=('description', 'name',)
+            Product.objects.filter(active=True),
+            fields=('description', 'name',)
         )
