8211ff2022c3ffb9154509be5e3ee55eee8f9890
Laura Maddox <ljgmaddox@gmail.com>
2015-05-05 17:08:21 -0700

    Pep8 utils.py

--- shop/utils.py
+++ shop/utils.py
@@ -8,7 +8,9 @@ from django.core.mail import send_mail
 from django.template.loader import render_to_string
 from django.shortcuts import get_object_or_404
 
-from shop.constants import DOMESTIC_SHIP_NATIONS, DOMESTIC_SHIP_RATES, INTL_SHIP_RATES
+from shop.constants import (
+    DOMESTIC_SHIP_NATIONS, DOMESTIC_SHIP_RATES, INTL_SHIP_RATES
+)
 from shop.models import OrderItem, ProdCategory, ProdVariation
 
 
@@ -24,16 +26,17 @@ def add_to_cart(cart, new_item):
             item['line_total'] = item['quantity'] * new_item['price']
             in_cart = True
             break
-    
+
     if not in_cart:
         cart['items'].append(new_item)
 
     return cart
 
+
 def calculate_cart_shipping(locale, item_total):
     """
     Calculate shipping cost based on cart total & customer's location
-    """  
+    """
     if not locale:
         # Default if customer has yet to set location
         return 0
@@ -42,10 +45,11 @@ def calculate_cart_shipping(locale, item_total):
             prices = DOMESTIC_SHIP_RATES
         else:
             prices = INTL_SHIP_RATES
-        
+
         for price in prices:
             if item_total > price[0]:
                 return price[1]
+
 
 def create_order(form_list, form_dict):
     """
@@ -56,13 +60,14 @@ def create_order(form_list, form_dict):
     payment_data = form_dict['payment'].data
     item_total = Decimal(payment_data['payment-item_total'])
     shipping_total = Decimal(payment_data['payment-shipping_total'])
-    
+
     order.item_total = item_total
     order.shipping_total = shipping_total
     order.order_total = item_total + shipping_total
     order.save()
-    
+
     return order
+
 
 def create_order_items(cart, order):
     """
@@ -81,6 +86,7 @@ def create_order_items(cart, order):
         )
         order_item.save()
 
+
 def generate_crumbs(path):
     """
     Create navigation crumbs for templates based on a category URL path
@@ -93,37 +99,42 @@ def generate_crumbs(path):
         else:
             parent = crumbs[-1]['category']
 
-        # Use parent to get each ProdCategory in case of duplicate category names
+        # Use parent to get ProdCategory in case of duplicate category names
         category = get_object_or_404(ProdCategory, slug=slug, parent=parent)
 
         crumbs.append({'category': category, 'path': category.path()})
 
     return crumbs
 
+
 def send_order_confirmation(order):
     """
-    Send order confirmation email to customer & email copy to shop to let us 
+    Send order confirmation email to customer & email copy to shop to let us
     know there's a new order
     """
-    html_message = render_to_string('email/order_thankyou.html', {'order': order})
-    text_message = render_to_string('email/order_thankyou_textonly.html', {'order': order})
+    html_message = render_to_string(
+        'email/order_thankyou.html', {'order': order}
+    )
+    text_message = render_to_string(
+        'email/order_thankyou_textonly.html', {'order': order}
+    )
     send_mail(
         subject='Uptown Hound Boutique order confirmation',
         message=text_message,
         from_email='Uptown Hound <orders@uptownhoundboutique.com>',
-        recipient_list=[order.customer_email], 
+        recipient_list=[order.customer_email],
         fail_silently=True,
         html_message=html_message
     )
-    # Send ours separately so the customer email doesn't show multiple recipients
     send_mail(
         subject='Uptown Hound Boutique order confirmation',
         message=text_message,
         from_email='Uptown Hound <orders@uptownhoundboutique.com>',
-        recipient_list=['orders@uptownhoundboutique.com'], 
+        recipient_list=['orders@uptownhoundboutique.com'],
         fail_silently=True,
         html_message=html_message
     )
+
 
 def update_cart_items(cart, updated_items):
     """
@@ -135,7 +146,7 @@ def update_cart_items(cart, updated_items):
     for item in cart_items:
         sku = item['sku']
         quantity = updates[sku]
-        
+
         item['quantity'] = quantity
         item['line_total'] = quantity * item['price']
 
@@ -143,6 +154,7 @@ def update_cart_items(cart, updated_items):
     cart['items'] = [item for item in cart_items if item['quantity'] != 0]
 
     return cart
+
 
 def update_totals(cart):
     """
@@ -152,8 +164,8 @@ def update_totals(cart):
 
     for item in cart['items']:
         item_total += item['line_total']
-        item_count += item['quantity']       
-    
+        item_count += item['quantity']
+
     shipping = calculate_cart_shipping(cart.get('locale', None), item_total)
 
     cart['item_total'] = item_total
@@ -162,5 +174,5 @@ def update_totals(cart):
 
     # Include an item count for the navbar shopping bag badge
     cart['item_count'] = item_count
-    
+
     return cart
