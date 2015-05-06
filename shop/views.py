4cffe978d4bb7cb408b2a0fb7f7ca40c10d909c7
Laura Maddox <ljgmaddox@gmail.com>
2015-05-05 17:14:04 -0700

    Pep8 views.py

--- shop/views.py
+++ shop/views.py
@@ -6,7 +6,9 @@ from collections import OrderedDict
 
 from django.contrib.formtools.wizard.views import SessionWizardView
 from django.http import HttpResponseRedirect, JsonResponse
-from django.shortcuts import redirect, render, render_to_response, get_object_or_404
+from django.shortcuts import (
+    redirect, render, render_to_response, get_object_or_404
+)
 
 import watson
 
@@ -49,6 +51,7 @@ def cart(request):
         'formset': formset, 'country_form': CartCountryForm
     })
 
+
 def cart_remove(request):
     """
     Handle GET or Ajax POST requests to remove a cart item
@@ -63,8 +66,8 @@ def cart_remove(request):
 
     # Update totals & re-save after removing the item
     request.session['cart'] = update_totals(cart)
-    
-    if request.is_ajax(): 
+
+    if request.is_ajax():
         return JsonResponse({
             'item_count': cart['item_count'],
             'item_total': cart['item_total'],
@@ -74,15 +77,16 @@ def cart_remove(request):
 
     return redirect('cart')
 
+
 def category(request, category_path):
     """
     Browse Products in a particular ProdCategory
     """
     crumbs = generate_crumbs(path=category_path)
-    
+
     # Get the leaf in the category path
     category = crumbs[-1]['category']
-    
+
     products = category.product_set.all().order_by('-date_added')
 
     return render(request, 'category.html', {
@@ -91,15 +95,20 @@ def category(request, category_path):
         'products': products,
     })
 
+
 def confirm_order(request, invoice_number):
     """
     Thank you page after successfully processing payment for an Order
     """
-    return render(request, 'checkout/confirm_order.html', {'invoice_number': invoice_number})
+    return render(request, 'checkout/confirm_order.html', {
+        'invoice_number': invoice_number
+    })
+
 
 def order_status(request):
     """
-    View for customer to look up status, contents, and tracking info for an Order
+    View for customer to look up status, contents, and tracking
+    info for an Order
     """
     form, order = OrderStatusForm(request.POST or None), None
 
@@ -111,11 +120,12 @@ def order_status(request):
             )
         except:
             form.add_error(None, 'We couldn\'t find that order.')
- 
+
     return render(request, 'order_status.html', {
         'form': form,
         'order': order
     })
+
 
 def product(request, product_slug):
     """
@@ -130,7 +140,7 @@ def product(request, product_slug):
         sku = form.cleaned_data['variation']
         quantity = form.cleaned_data['quantity']
         variation = ProdVariation.objects.get(sku=sku)
-        
+
         cart_item = {
             'image': product.main_img.image.url,
             'line_total': float(variation.price * quantity),
@@ -141,17 +151,18 @@ def product(request, product_slug):
             'url': product.slug,
             'size': variation.size
         }
-        
+
         cart = request.session.get('cart', {'items': []})
         request.session['cart'] = update_totals(add_to_cart(cart, cart_item))
 
-    return render(request, 'product.html', { 
+    return render(request, 'product.html', {
         'crumbs': generate_crumbs(product.first_child().path()),
         'form': form,
         'cart_item': cart_item,
         'product': product,
         'variations': product.variations.all()
     })
+
 
 def search(request):
     """
@@ -180,12 +191,12 @@ class OrderWizard(SessionWizardView):
         """
         cart = self.request.session['cart']
 
-        # Recalculate shipping based on delivery address before moving to payment form
+        # Recalculate shipping based on form data before continuing to payment
         if step == 'payment':
-            shipping_info = self.get_cleaned_data_for_step('shipping')            
+            shipping_info = self.get_cleaned_data_for_step('shipping')
             cart['locale'] = shipping_info['customer_nation']
             cart = update_totals(cart)
-            self.request.session['cart'] = cart  
+            self.request.session['cart'] = cart
 
         initial_dict = {
             'payment': {
@@ -220,33 +231,38 @@ class OrderWizard(SessionWizardView):
         """
         Add context variables to checkout step templates
         """
-        context = super(OrderWizard, self).get_context_data(form=form, **kwargs)
-        
+        context = super(OrderWizard, self).get_context_data(
+            form=form, **kwargs
+        )
+
         button_text = {
             'shipping': 'Continue to payment',
             'payment': 'Place order',
         }
         context.update({
             'button_text': button_text[self.steps.current],
-            'shipping_info': self.get_cleaned_data_for_step('shipping')
-,
+            'shipping_info': self.get_cleaned_data_for_step('shipping'),
         })
         return context
 
     def render_done(self, form, **kwargs):
         """
-        Override render_done to skip form re-validation to avoid processing
-        the Stripe payment a second time in the clean() method for OrderPaymentForm
+        Override render_done to skip form re-validation to avoid processing the
+        Stripe payment a second time in the clean() method for OrderPaymentForm
         """
         final_forms = OrderedDict()
 
         for form_key in self.get_form_list():
-            form_obj = self.get_form(step=form_key,
+            form_obj = self.get_form(
+                step=form_key,
                 data=self.storage.get_step_data(form_key),
-                files=self.storage.get_step_files(form_key))
+                files=self.storage.get_step_files(form_key)
+            )
             final_forms[form_key] = form_obj
 
-        done_response = self.done(final_forms.values(), form_dict=final_forms, **kwargs)
+        done_response = self.done(
+            final_forms.values(), form_dict=final_forms, **kwargs
+        )
         self.storage.reset()
         return done_response
 
