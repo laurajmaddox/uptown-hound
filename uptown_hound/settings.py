d433e87112684c5966b2f579911663d370b79d6a
Laura Maddox <ljgmaddox@gmail.com>
2015-05-14 16:32:24 -0700

    Dev db settings

--- uptown_hound/settings.py
+++ uptown_hound/settings.py
@@ -67,6 +67,14 @@ ROOT_URLCONF = 'uptown_hound.urls'
 WSGI_APPLICATION = 'uptown_hound.wsgi.application'
 
 
+# Development database settings
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
 # Internationalization
 # https://docs.djangoproject.com/en/1.7/topics/i18n/
 
